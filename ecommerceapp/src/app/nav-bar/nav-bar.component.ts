422b108d71b9fd609a4219e373ead678cf667e51
shraddhamira <shraddha.s.mirashi@gmail.com>
2018-07-25 20:00:54 +0530

    Cart Menu to Icon Conversion

--- ecommerceapp/src/app/nav-bar/nav-bar.component.ts
+++ ecommerceapp/src/app/nav-bar/nav-bar.component.ts
@@ -2,20 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
 import { AuthService } from '../providers/auth.service';
+import { CartService } from '../providers/cart.service';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-  constructor(public auth : AuthService) {}
-
+  constructor(public auth: AuthService,private cartService:CartService ) { }
+  cartData: any[];
   ngOnInit() {
   }
-  
+
   logout() {
     this.auth.logout();
   }
 
+  getCurrentCartDetails() {
+    this.cartData = [];
+    this.cartService.getExistingProducts().subscribe((res) => {
+      let jsonRecord = res.json();
+      if (jsonRecord) {
+        let keys = Object.keys(jsonRecord);
+        this.cartData = keys.map(function (key) {
+          return { key: key, value: jsonRecord[key] }
+        });
+       // this.getAllProductsData();
+      } else {
+
+      }
+    }, (err) => {
+      console.log(err);
+    })
+  }
 
 }
